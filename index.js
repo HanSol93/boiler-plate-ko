ab0baabfad23b9c5f8b7c12e6cb002b17fb9bd58
elody-han <elody.han@kakaoenterprise.com>
2021-01-26 11:58:42 +0900

    ADD: config mongoURI

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://sol:3653@biilerplate.aib0s.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
+// application/x-www-form-urlencoded 데이터를 받아올 수 있게 해줌
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json 데이터를 받아올 수 있게 해줌
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World! 안농 ㅎㅎ");
+  res.send("Hello World! 안농 ㅎㅎㅋ");
+});
+
+app.post("/register", (req, res) => {
+  // 화원 가입 시 필요한 정보 client에서 가져오면 그 값들을 DB에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
